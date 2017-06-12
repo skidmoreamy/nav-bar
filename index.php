5b369431dc7b4266943df9e9992a6e5ca5fac472
amy skidmore <askidmore1@cnm.edu>
2017-06-11 20:26:38 -0600

    initial

--- index.php
+++ index.php
@@ -1,28 +1,35 @@
 <!<header>
-	<nav class="navbar navbar-inverse">
+	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
-
-			<!-- MOBILE DISPLAY -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+						  data-target=".navbar-ex1-collapse">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="">PetFosterABQ</a>
+				<a href="#page-top" class="navbar-brand page-scroll">
+					<div class="name"><span>PetFosterABQ</span>
+					</div>
+				</a>
 			</div>
-
-			<!-- COLLECT LINKS, FORMS AND OTHER CONTENT FOR TOGGLING -->
-			<div class="collapse navbar-collapse" id="nav-bar">
-
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href=""><i class="fa fa-home"></i></a></li>
-					<li><a href="sign-in">Sign-In</a></li>
-					<li><a href="sign-out">Sign-Out</a></li>
+					<li>
+						<a class="page-scroll" href="about.php" style="color: #0d05fa">About</a>
+					</li>
+					<li>
+						<a class="page-scroll" href="sign-up.php" style="color: #0d05fa">Sign-Up</a>
+					</li>
+					<li>
+						<a class="page-scroll" href="sign-in.php" style="color: #0d05fa">Sign-In</a>
+					</li>
+					<li>
+						<a class="page-scroll" href="sign-out.php" style="color: #0d05fa">Sign-Out</a>
+					</li>
 				</ul>
-
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
+			</div>
+		</div>
+	</nav><!-- /.navbar-default -->
 </header>
