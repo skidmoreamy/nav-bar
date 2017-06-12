7f3d92626d051b352fb1d6bb13e62e101bd321ca
amy skidmore <askidmore1@cnm.edu>
2017-06-11 20:10:06 -0600

    initial

--- index.php
+++ index.php
@@ -1,59 +1,28 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
-		xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
-		xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<!<header>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
 
-	<head>
-		<meta charset="utf-8"/>
-		<!--Tells the browser that the page is written in UTF-8 unicode. This is for internationalization.-->
+			<!-- MOBILE DISPLAY -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="">PetFosterABQ</a>
+			</div>
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<!--This tag is for Internet Explorer, and it allows us to specify what version of IE to render page in. "IE=edge" tells IE to display our content in the highest mode available, which avoids "IE Compatibility Mode" bugs.-->
+			<!-- COLLECT LINKS, FORMS AND OTHER CONTENT FOR TOGGLING -->
+			<div class="collapse navbar-collapse" id="nav-bar">
 
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<!--This is the responsive meta tag. This is used to scale and size our content relative to the "viewport" - which is essentially is the visible portion of screen itself. This tag also sets the inital zoom scale to 1:1. This <meta> tag is required when creating a responsive web site.-->
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href=""><i class="fa fa-home"></i></a></li>
+					<li><a href="sign-in">Sign-In</a></li>
+					<li><a href="sign-out">Sign-Out</a></li>
+				</ul>
 
-		<meta name="description" content="PWP">
-		<meta name="author" content="Amy Skidmore">
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme compiled/min/css-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-				crossorigin="anonymous"/>
-
-		<!-- Font Awesome -->
-		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-				rel="stylesheet"/>
-		<!--Google Fonts-->
-		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Fredericka+the+Great|Merriweather|Nunito:800" rel="stylesheet">
-		<!--custom css-->
-		<link rel="stylesheet" href="../nav-bar/css/css-style.css" type="text/css">
-	<body class="sfooter">
-		<div class="sfooter-content">
-
-			<!-- insert header and navbar -->
-
-			<main class="bg p-t-nav">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8 col-md-offset-4">
-							<div class="jumbotron text-right">
-								<div class="fa fa-copyright" aria-hidden="true">PetRescue 2017</div>
-								<div class="container-fluid">
-									<h1>Title text.</h1>
-									<p class="lead">Sample text.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-			</main>
-		</div><!--/.sfooter-content-->
-
-		<!-- insert footer -->
-
-	</body>
-</html>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
